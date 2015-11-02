e95f1a3ff00b03067fd6bdae166642a3303ad5f3
Antônio Roberto <forevertonny@gmail.com>
2015-11-02 12:46:22 -0200

    some styles adjust

--- src/server.js
+++ src/server.js
@@ -2,30 +2,37 @@ import Hapi from 'hapi';
 import good from 'good';
 import api from './api';
 
-var server = new Hapi.Server();
+let server = new Hapi.Server(),
+    serverRegisterOptions = () => {
+        return [
+            {
+                register: good,
+                options: {
+                    reporters: [{
+                        reporter: 'good-console',
+                        events: {log: '*', response: '*', error: '*'}
+                    }]
+                }
+            },
+            {
+                register: api
+            }
+        ];
+    }(),
+    serverErrorHandler = (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            server.start(() => {
+                console.log('Server started at: ' + server.info.uri); // jshint ignore:line
+            });
+        }
+    }; 
 
 server.connection({
-  port: process.env.PORT || 3000,
-  host: '0.0.0.0'
+    port: process.env.PORT || 3000,
+    host: '0.0.0.0'
 });
 
-server.register([{
-  register: good,
-  options: {
-    reporters: [{
-      reporter: 'good-console',
-      events: {log: '*', response: '*', error: '*'}
-    }]
-  }
-}, {
-  register: api
-}], function(err) {
-  'use strict';
-  if (err) {
-    console.error(err);
-  } else {
-    server.start(function() {
-      console.log('Server started at: ' + server.info.uri); // jshint ignore:line
-    });
-  }
-});
+server.register(serverRegisterOptions, serverErrorHandler);
+
