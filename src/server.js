1a080ea3e4ab9942983ccb186a69e6e678f75924
Lucas Pirola <lucaspirola@gmail.com>
2016-06-20 17:52:33 -0300

    Fix bug with not found server var

--- src/server.js
+++ src/server.js
@@ -45,14 +45,13 @@ app.set('port', port)
  //openssl req -new -key privatekey.pem -out certrequest.csr
  //openssl x509 -req -in certrequest.csr -signkey privatekey.pem -out certificate.pem
 
-if (app.get('env') === 'development') {
+let server = http.createServer(app)
+if (process.env.NODE_ENV === 'development') {
   var options = {
     key: fs.readFileSync(__dirname + '/certs/privatekey.pem'),
     cert: fs.readFileSync(__dirname + '/certs/certificate.pem')
   };
-  let server = https.createServer(options, app)
-} else {
-  let server = http.createServer(app)
+  server = https.createServer(options, app)
 }
 
 /**
